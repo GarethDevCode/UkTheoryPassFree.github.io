77974f91e47aa4693524121f2c6eeba28c6efc18
PrecisionGamingUK <Precision.Gaming@outlook.com>
2022-09-01 00:10:00 +0100

    Second Major Upload
    Added "viewMore" buttons

--- Script.js
+++ Script.js
@@ -1,1 +1,22 @@
 // JavaScript source code
+
+
+// Read more button
+function viewMoreFunction(dotID, textID, buttonID) {
+    var dots = document.getElementById(dotID);
+    var moreText = document.getElementById(textID);
+    var btnText = document.getElementById(buttonID);
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Show More";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Show Less";
+        moreText.style.display = "inline";
+    }
+}
+// End
+
+
